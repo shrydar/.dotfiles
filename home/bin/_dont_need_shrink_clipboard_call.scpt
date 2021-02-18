dc3667c4bff6b13bad09b3926db13090a232182f
Christopher Phillips <shrydar@jaruth.com>
2021-02-18 13:36:06 +0800

    Nuke unused home/bin folder

--- home/bin/_dont_need_shrink_clipboard_call.scpt
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/osascript
-do shell script "/Users/swrathal/bin/shrink_clipboard.sh"
-
--- home/bin/login_script.sh
+++
@@ -1,4 +0,0 @@
-# Increase the 2 kernel memory management values for postgres from very small, to ok values:
-sudo sysctl -w kern.sysv.shmall=65536
-sudo sysctl -w kern.sysv.shmmax=16777216
-
